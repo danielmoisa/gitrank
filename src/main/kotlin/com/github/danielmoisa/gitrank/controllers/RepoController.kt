da7b8d3fe7eb4cce3881493d823494d4385fd7e7
Daniel Moisa <danimoisa93@gmail.com>
2022-12-08 14:20:21 +0200

    add repo mutation

--- src/main/kotlin/com/github/danielmoisa/gitrank/controllers/RepoController.kt
+++ src/main/kotlin/com/github/danielmoisa/gitrank/controllers/RepoController.kt
@@ -3,9 +3,9 @@ package com.github.danielmoisa.gitrank.controllers
 import com.github.danielmoisa.gitrank.models.Repo
 import com.github.danielmoisa.gitrank.repositories.RepoRepository
 import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
-import java.util.*
 
 @Controller
 class RepoController(val repoRepository: RepoRepository) {
@@ -13,6 +13,9 @@ class RepoController(val repoRepository: RepoRepository) {
     fun getAllRepos(): List<Repo> {
         return repoRepository.findAll()
     }
-
+    @MutationMapping(name = "createRepo")
+    fun createRepo(@Argument(name = "repo") repo: Repo): Repo {
+        return repoRepository.save(repo)
+    }
 
 }
